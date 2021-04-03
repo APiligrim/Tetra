d0093b0f3cfa8cc58008ae6cf30c1a7224ad49a5
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2021-04-03 21:17:54 +0530

    add fabric and set node version

--- server/index.js
+++ server/index.js
@@ -12,30 +12,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 require("./routes/user.js")(app);
 // require("./routes/item.js")(app);
 
-app.put("/items/:itemcode", (req, res) => {
-  console.log(req.que);
-  const { itemcode } = req.params;
-  if (itemcode === "1") {
-    res.status(200).json({
-      success: true,
-      item_code: "1",
-      item_name: "Shirt",
-    });
-  } else if (itemcode === "2") {
-    res.status(200).json({
-      success: true,
-      item_code: "2",
-      item_name: "Bag",
-    });
-  } else {
-    res.status(200).json({
-      success: false,
-    });
-  }
-});
-
-// GET, POST
-
 app.listen(5000, () => console.log("Server @ port 5000"));
 module.exports = app;
 
